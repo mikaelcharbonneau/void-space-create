ed274c2fa0b735e6d9ca22e6fd83b2bef2f504b0
mikaelcharbonneau <153523692+mikaelcharbonneau@users.noreply.github.com>
2025-05-27 11:58:19 -0500

    Fix Router and HMR Issues

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     hmr: {
       timeout: 120000,
-      clientPort: 443,
-      host: 'localhost'
+      host: '0.0.0.0',
+      protocol: 'ws',
+      clientPort: 443
     },
     proxy: {
       '/api': {
